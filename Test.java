c04c8dc7d8ac0e8f50cd2acdced11bb127e08fd8
Vic <h88001166@gmail.com>
2022-09-30 10:44:15 +0800

    input

--- Test.java
+++ Test.java
@@ -1,17 +1,18 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] avgs) {
-        int num = 1;
-        printStr("" + num);
-        num = 2;
-        printStr("" + num);
-        num = num + 1;
-        printStr("" + num);
-        num += 1;
-        printStr("" + num);
-        ++ num;
-        printStr("" + num);
-        num ++;
-        printStr("" + num);
+        int num1;
+        int num2;
+        int sum;
+
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter first integer: ");
+        num1 = input.nextInt();
+        System.out.printf("Enter second integer: ");
+        num2 = input.nextInt();
+        sum = num1 + num2;
+        System.out.println("The sum is " + sum);
     }
 
     public static void printStr(String str) {
