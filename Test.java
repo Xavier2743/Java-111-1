507f02edb8a8bebd088d2321d7e1b7db36a5fe7d
Vic <h88001166@gmail.com>
2022-09-30 08:55:46 +0800

    Third Version

--- Test.java
+++ Test.java
@@ -2,5 +2,10 @@ public class Test {
     public static void main(String[] avgs) {
         System.out.println(avgs[0]);
         System.out.println("Second Version!");
+        printStr("Vic");
+    }
+
+    public static void printStr(String str) {
+        System.out.println("My Name is " + str + ".");
     }
 }
