5ed90053dfb3012c4dd3fbc0eba4e9228cc5d908
Vic <h88001166@gmail.com>
2022-10-07 08:35:43 +0800

    test

---
+++ Test3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] avgs) {
+        int num;
+
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter your score: ");
+        num = input.nextInt();
+
+        if (num >= 0 && num <= 100) {
+            if (num >= 90) {
+                System.out.println("A");
+            }
+            else if (num >= 80) {
+                System.out.println("B");
+            }
+            else if (num >= 70) {
+                System.out.println("C");
+            }
+            else if (num >= 60) {
+                System.out.println("D");
+            }
+            else {
+                System.out.println("E");
+            }
+        }
+        else {
+            System.out.printf("Input error.");
+        }
+    }
+}
