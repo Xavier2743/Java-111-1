e0508dc7fb16783c366994c171040f097eec76f6
vic <h88001166@gmail.com>
2022-10-07 10:54:56 +0800

    while 9-0

---
+++ Test6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test6 {
+    public static void main(String[] avgs) {
+        int grade = 9;
+
+        while (grade >= 0) {
+            System.out.printf("%d%n", grade);
+            grade--;
+        }
+    }
+}
