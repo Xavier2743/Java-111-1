ec510f60b9eef3984fc0f59ecc04869bacfa7c35
vic <h88001166@gmail.com>
2022-10-07 11:22:49 +0800

    single while space+number

--- Test6.java
+++ Test6.java
@@ -2,18 +2,14 @@ import java.util.Scanner;
 
 public class Test6 {
     public static void main(String[] avgs) {
-        int num;
         int grade = 0;
+        String space = "";
         
         while (grade < 10) {
-            num = 0;
-            while (num < grade) {
-                System.out.printf(" ");
-                num ++;
-            }
-
+            System.out.printf(space);
             System.out.println(grade);
             grade ++;
+            space += " ";
         }
     }
 }
