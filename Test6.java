3b7df600fee57c7a4d70087d92715d9a4bd68f71
vic <h88001166@gmail.com>
2022-10-07 11:16:05 +0800

    double while space+number

--- Test6.java
+++ Test6.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class Test6 {
     public static void main(String[] avgs) {
+        int num;
         int grade = 0;
-
+        
         while (grade < 10) {
-            System.out.printf("%d%n", grade);
-            grade++;
+            num = 0;
+            while (num < grade) {
+                System.out.printf(" ");
+                num ++;
+            }
+
+            System.out.println(grade);
+            grade ++;
         }
     }
 }
