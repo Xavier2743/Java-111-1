24b731b1ad9a4c4c649181b04a5fe7c63d6608f2
vic <h88001166@gmail.com>
2022-10-14 08:23:01 +0800

    while *

---
+++ Test0501.java
@@ -0,0 +1,12 @@
+public class Test0501 {
+    public static void main(String[] avgs) {
+        int num = 1;
+        String ch = "*";
+        
+        while (num < 6) {
+            System.out.println(ch);
+            ch += "*";
+            num ++;
+        }
+    }
+}
