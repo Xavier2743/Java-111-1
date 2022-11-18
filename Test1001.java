4c9393c4c709ec1db7ec53510386aaaacf82c692
vic <h88001166@gmail.com>
2022-11-18 08:58:09 +0800

    static

---
+++ Test1001.java
@@ -0,0 +1,15 @@
+public class Test1001 {
+    public static void main(String[] args) {
+        int a;
+        int k = 7;
+
+        System.out.printf("%d%n", k);
+        a = add(k, 4);
+        System.out.printf("%d%n", a);
+    }
+
+    public static int add(int x, int y) {
+        int t = x + y;
+        return t;
+    }
+}
