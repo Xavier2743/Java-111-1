c1220f53d99aa158dfb9eed090adfff5987c7c6b
vic <h88001166@gmail.com>
2022-11-18 11:20:15 +0800

    （方形*） - （菱形空白），可自定行數

--- Test1003.java
+++ Test1003.java
@@ -1,15 +1,15 @@
 public class Test1003 {
     public static void main(String[] args) {
-        int n = 0;
-        for (int i = 1; i <= 9; i++) {
-            if (i > 5) {
-                n = 10 - i;
-            }
-            else {
-                n = i;
-            }
-            for (int j = 1; j <= 11; j++) {
-                if (j > 6 - n && j < 6 + n) {
+        int row = 5, n;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row + 2; j++) {
+                if (i > (row + 1) / 2) {
+                    n = row + 1 - i;
+                }
+                else {
+                    n = i;
+                }
+                if (j > (row + 3) / 2 - n && j < (row + 3) / 2 + n) {
                     System.out.print(" ");                    
                 }
                 else {
