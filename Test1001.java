7a676a5eb79fe3cc620c731a1744ee0867480af0
vic <h88001166@gmail.com>
2022-11-18 09:19:53 +0800

    class 在另一個檔案

--- Test1001.java
+++ Test1001.java
@@ -1,12 +1,15 @@
 public class Test1001 {
     public static void main(String[] args) {
-        int a;
+        int a, b;
         int k = 7;
 
         System.out.printf("%d%n", k);
-        Test1001_1 test10 = new Test1001_1();
-        a = test10.add(k, 4);
+        Test1001_1 test0 = new Test1001_1();
+        a = test0.add(k, 4);
         System.out.printf("%d%n", a);
+        Test1002 test1 = new Test1002();
+        b = test1.sub(k, 4);
+        System.out.printf("%d%n", b);
     }
 
 }
---
+++ Test1002.java
@@ -0,0 +1,6 @@
+class Test1002 {
+    public int sub(int x, int y) {
+        int t = x - y;
+        return t;
+    }
+}
