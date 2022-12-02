670d8ec0be1c4037047f045d48889135d8be957d
vic <h88001166@gmail.com>
2022-12-02 11:25:47 +0800

    array-1

--- Test1201_User.java
+++ Test1201_User.java
@@ -8,7 +8,7 @@ public class Test1201_User {
             myCar[i].wheel[1] = i + 1;
             myCar[i].wheel[2] = i + 2;
             myCar[i].wheel[3] = i + 3;
-            System.out.printf("%d\t%d\t%d\t%d\n", myCar[i].wheel[0], myCar[i].wheel[1], myCar[i].wheel[2], myCar[i].wheel[3]);
+            System.out.printf("%d\t%d\t%d\t%d\n", myCar[i].wheel[0], myCar[i].wheel[1], myCar[i].wheel[3], myCar[i].wheel[3]);
             int z = myCar[i].launch(7);
             System.out.println(z);
         }
--- Test1202_Car.java
+++ Test1202_Car.java
@@ -1,5 +1,6 @@
 public class Test1202_Car {
     public int[] wheel = new int[4];
+    
 
     public int launch(int key) {
         if (key == 7) {
