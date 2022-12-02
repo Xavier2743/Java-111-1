aaa0459b1c7b1b81452dbde0d8ea7dd95df4b527
vic <h88001166@gmail.com>
2022-12-02 09:58:11 +0800

    array

---
+++ Test1202_Car.java
@@ -0,0 +1,12 @@
+public class Test1202_Car {
+    public int[] wheel = new int[4];
+
+    public int launch(int key) {
+        if (key == 7) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
